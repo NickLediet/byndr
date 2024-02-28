365c614a8ed2b56cc051b4ffe8cd83f28b604b11
Nicholas Lediet <nicklediet@gmail.com>
2024-02-28 00:53:14 -0500

    Add libs/ui

--- libs/ui/vite.config.ts
+++ libs/ui/vite.config.ts
@@ -7,7 +7,7 @@ import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 
 export default defineConfig({
   root: __dirname,
-  cacheDir: '../../node_modules/.vite/libs/components',
+  cacheDir: '../../node_modules/.vite/libs/ui',
 
   plugins: [
     react(),
@@ -27,7 +27,7 @@ export default defineConfig({
   // Configuration for building your library.
   // See: https://vitejs.dev/guide/build.html#library-mode
   build: {
-    outDir: '../../dist/libs/components',
+    outDir: '../../dist/libs/ui',
     reportCompressedSize: true,
     commonjsOptions: {
       transformMixedEsModules: true,
@@ -35,7 +35,7 @@ export default defineConfig({
     lib: {
       // Could also be a dictionary or array of multiple entry points.
       entry: 'src/index.ts',
-      name: 'components',
+      name: 'ui',
       fileName: 'index',
       // Change this to the formats you want to support.
       // Don't forget to update your package.json as well.
@@ -57,7 +57,7 @@ export default defineConfig({
 
     reporters: ['default'],
     coverage: {
-      reportsDirectory: '../../coverage/libs/components',
+      reportsDirectory: '../../coverage/libs/ui',
       provider: 'v8',
     },
   },
