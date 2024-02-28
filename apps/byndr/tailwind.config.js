04efbc8d34af6a45453eb1f4634ba41ff43147ca
Nicholas Lediet <nicklediet@gmail.com>
2024-02-28 01:19:56 -0500

    Wire up tailwind with lib/ui and apps/byndr

---
+++ apps/byndr/tailwind.config.js
@@ -0,0 +1,16 @@
+const { createGlobPatternsForDependencies } = require('@nx/react/tailwind');
+const { join } = require('path');
+/** @type {import('tailwindcss').Config} */
+const baseConfig = require(`${__dirname}/../../libs/ui/tailwind.config.js`)
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  ...baseConfig,
+  content: [
+    join(
+      __dirname,
+      '{src,pages,components,app}/**/*!(*.stories|*.spec).{ts,tsx,html}'
+    ),
+    ...createGlobPatternsForDependencies(__dirname),
+  ],
+};
--- libs/ui/src/lib/ui.tsx
+++ libs/ui/src/lib/ui.tsx
@@ -6,7 +6,8 @@ export interface UiProps {}
 export function Ui(props: UiProps) {
   return (
     <div className={styles['container']}>
-      <h1>Welcome to Ui!</h1>
+      <h1 className="text-orange-500">Welcome to Ui!</h1>
+      <p className="h3 text-orange-200">Smaller text! (how exciting)</p>
     </div>
   );
 }
