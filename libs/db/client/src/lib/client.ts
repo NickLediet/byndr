e1e05023089fd416ff91c6b74644d7d0a4ed5c9a
Nicholas Lediet <nicklediet@gmail.com>
2024-06-28 01:12:46 -0400

    Refactor @byndr/import-tools to app and remove old lib code officially'

--- libs/db/client/src/lib/client.ts
+++ libs/db/client/src/lib/client.ts
@@ -3,25 +3,25 @@ import postgres from 'postgres'
 import { drizzle } from 'drizzle-orm/postgres-js'
 
 dotenv.config()
-const { 
-  POSTGRES_USER,
-  POSTGRES_PASSWORD, 
-  POSTGRES_DB,
-  POSTGRES_HOST,
-  POSTGRES_PORT
-} = process.env
-
-if (!POSTGRES_USER || !POSTGRES_PASSWORD || !POSTGRES_DB || !POSTGRES_HOST || !POSTGRES_PORT) {
-  throw new Error('Missing required environment variables')
-}
-
 export function createDatabaseClient() {
+  const { 
+    POSTGRES_USER,
+    POSTGRES_PASSWORD, 
+    POSTGRES_DB,
+    POSTGRES_HOST,
+    POSTGRES_PORT
+  } = process.env
+
+  if (!POSTGRES_USER || !POSTGRES_PASSWORD || !POSTGRES_DB || !POSTGRES_HOST || !POSTGRES_PORT) {
+    throw new Error('Missing required environment variables')
+  }
+
   const pgClient = postgres({
     user: POSTGRES_USER,
     database: POSTGRES_DB,
     password: POSTGRES_PASSWORD,
     host: POSTGRES_HOST,
-    port: parseInt(POSTGRES_PORT!)
+    port: parseInt(POSTGRES_PORT)
   })
 
   return {
--- libs/import-tools/src/lib/import-tools.spec.ts
+++
@@ -1,7 +0,0 @@
-import { importTools } from './import-tools';
-
-describe('importTools', () => {
-  it('should work', () => {
-    expect(importTools()).toEqual('import-tools');
-  });
-});
