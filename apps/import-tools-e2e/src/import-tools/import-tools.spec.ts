d5a347d76990d379279a6ef34b514b91617ceb06
Nicholas Lediet <nicklediet@gmail.com>
2024-07-02 22:47:56 -0400

    Fix e2e tests for @byndr/import-tools

--- apps/import-tools-e2e/src/import-tools/import-tools.spec.ts
+++ apps/import-tools-e2e/src/import-tools/import-tools.spec.ts
@@ -4,18 +4,13 @@ import { join } from 'path';
 describe('CLI tests', () => {
   it('should successfully run the CLI with no errors', () => {
     try {
-      const cliPath = join(process.cwd(), 'dist/apps/import-tools');
+      // when running `nx test @byndr/import-tools-e2e` the process.cwd() is the THIS DIRECTORY
+      const cliPath = join(process.cwd(), '../../dist/apps/import-tools');
       const output = execSync(`node ${cliPath} --help`).toString();
       expect(output).toContain('Import tools for Byndr');
     } catch (error) {
       console.error(error);
       expect(true).toBe(false);
     }
-  // it('should print a message', () => {
-  //   const cliPath = join(process.cwd(), 'dist/apps/import-tools');
-
-  //   const output = execSync(`node ${cliPath}`).toString();
-
-  //   expect(output).toMatch(/Hello World/);
   });
 });
