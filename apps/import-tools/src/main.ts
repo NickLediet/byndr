1a2bc86b81b709383cdd254199b491b1f03e54c3
Nicholas Lediet <nicklediet@gmail.com>
2024-07-02 21:18:39 -0400

    Add debug command to validate redis container is working

--- apps/import-tools/src/main.ts
+++ apps/import-tools/src/main.ts
@@ -4,13 +4,30 @@ import { NewEntry, lists, entries } from '@byndr/db-schema'
 import { readFile } from 'fs/promises'
 import { camelCase, isArray } from 'lodash'
 import { sql } from 'drizzle-orm'
-
+import { createClient } from 'redis'
+import 'dotenv/config'
 const program = new Command()
 
 program
   .name('byndr')
   .description('Import tools for Byndr')
   .version('0.0.1')
+
+program.command('debug')
+  .description('Debugging command for testing, will be removed in production')
+  .action(async () => {
+    console.log('DEBUG: Connecting to database...')
+     const client = await createClient({
+      password: process.env.REDIS_PASSWORD
+     })
+      .on('error', (err) => console.log('Error:', err))
+      .connect()
+    
+    const value = await client.GET('byndr_test_key')
+    console.log('DEBUG: Value:', value)
+
+    client.disconnect()
+  })
 
 program.command('list:create')
   .description('Create a new list in Byndr')
