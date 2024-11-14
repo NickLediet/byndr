359b26d13b1987e25c8084ad912d4e87b02c24d2
Nicholas Lediet <nicklediet@gmail.com>
2024-11-14 14:02:18 -0500

    Get nest.js running with graphql added

--- apps/services/supergraph/src/app/app.module.ts
+++ apps/services/supergraph/src/app/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
-
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'node:path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      playground: process.env.NODE_ENV !== 'production',
+      typePaths: ['./**/*.graphqls'],
+      definitions: {
+        path: join(process.cwd(), 'src/generated/graphql.ts'),
+      }
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
